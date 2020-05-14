6fa5dc8889dffcf41f4dcad2e49bcd026f9581da
Eva Beyen <eebeyen@ucdavis.edu>
2020-05-14 16:57:38 -0700

    Adding in info for blue picks

--- dataprepscript.R
+++ dataprepscript.R
@@ -1,34 +1,30 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-d = read_xlsx("C:/Users/All Other Users/Documents/germdata/Germination_survey_field_data_20200413_EEB.xlsx")
+d = read_xlsx("C:/Users/All Other Users/Documents/germdata/Germination_survey_field_data_withgermcolor_EEB.xlsx")
 
 head(d)
 
 e=d %>%
-  select(-orange.m,-yellow.m, -green.m., -`collected by`)
+  select(-orange.m,-yellow.m, -green.m., -blue.m, -`collected by`)
 
 e$orange = as.numeric(e$orange.p)
 e$yellow = as.numeric(e$yellow.p)
 e$green = as.numeric(e$green.p)
+e$blue = as.numeric (e$blue.p)
 e$quadrat = as.character(e$quadrat)
 head(e)
 
 # Table with color counts
 f = e %>%
-  select(-orange.p,-yellow.p,-green.p) %>%
-  pivot_longer(cols=c(orange, yellow, green),
+  select(-orange.p,-yellow.p,-green.p, -blue.p) %>%
+  pivot_longer(cols=c(orange, yellow, green, blue),
                names_to='color',
                values_to='colorcount') %>%
   group_by(plot, quadrat, color) %>%
   summarize('n'= max(colorcount, 
                      na.rm=TRUE))
 
-# Dealing with absent 1/24 data for lower site`
-g = f %>%
-  filter(n != '-Inf')
-head(g)
-g$quadratnum = as.numeric(g$quadrat)
 
 potato = g %>%
   uncount(n) %>%
@@ -37,6 +33,9 @@ potato = g %>%
   as.data.frame()
 
 write.xlsx(potato,
-           "C:/Users/All Other Users/Documents/germdata/AnnualCensusPrefill2020.xlsx")
+           "C:/Users/All Other Users/Documents/germdata/AnnualCensusPrefill2020updated.xlsx")
 ?write.xlsx
+
+
+head(potato)
 
