40d599e36128ed013074f8b2e5cc23c2c33a6d25
eebeyen <45860853+eebeyen@users.noreply.github.com>
2020-05-14 17:04:08 -0700

    Update dataprepscript.R

--- dataprepscript.R
+++ dataprepscript.R
@@ -6,18 +6,19 @@ d = read_xlsx("C:/Users/All Other Users/Documents/germdata/Germination_survey_fi
 head(d)
 
 e=d %>%
-  select(-orange.m,-yellow.m, -green.m., -`collected by`)
+  select(-orange.m,-yellow.m, -green.m., -blue.m -`collected by`)
 
 e$orange = as.numeric(e$orange.p)
 e$yellow = as.numeric(e$yellow.p)
 e$green = as.numeric(e$green.p)
+e$blue = as.numeric(e$blue.p)
 e$quadrat = as.character(e$quadrat)
 head(e)
 
 # Table with color counts
 f = e %>%
-  select(-orange.p,-yellow.p,-green.p) %>%
-  pivot_longer(cols=c(orange, yellow, green),
+  select(-orange.p,-yellow.p,-green.p, -blue.p) %>%
+  pivot_longer(cols=c(orange, yellow, green, blue),
                names_to='color',
                values_to='colorcount') %>%
   group_by(plot, quadrat, color) %>%
