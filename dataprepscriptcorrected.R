0c93fafba90d020961c31a0f99951c6ddecd017c
Eva Beyen <eebeyen@ucdavis.edu>
2020-05-14 17:17:59 -0700

    fixed issues with blue

---
+++ dataprepscriptcorrected.R
@@ -0,0 +1,44 @@
+#install.packages("tidyverse")
+library(tidyverse)
+library(readxl)
+d = read_xlsx("C:/Users/All Other Users/Documents/germdata/Germination_survey_field_data_withgermcolor_EEB.xlsx")
+
+head(d)
+
+e=d %>%
+  select(-orange.m,-yellow.m, -green.m., -blue.m, -`collected by`)
+
+e$orange = as.numeric(e$orange.p)
+e$yellow = as.numeric(e$yellow.p)
+e$green = as.numeric(e$green.p)
+e$blue = as.numeric (e$blue.p)
+e$quadrat = as.character(e$quadrat)
+head(e)
+
+# Table with color counts
+f = e %>%
+  select(-orange.p,-yellow.p,-green.p, -blue.p) %>%
+  pivot_longer(cols=c(orange, yellow, green, blue),
+               names_to='color',
+               values_to='colorcount') %>%
+  group_by(plot, quadrat, color) %>%
+  summarize('n'= max(colorcount, 
+                     na.rm=TRUE))
+f$quadratnum = as.numeric(f$quadrat)
+
+potato = f %>%
+  uncount(n) %>%
+  arrange(plot,quadratnum,color) %>% 
+  select(-quadratnum) %>%
+  as.data.frame()
+
+potato %>%
+  filter(color=='blue')
+
+write.xlsx(potato,
+           "C:/Users/All Other Users/Documents/germdata/AnnualCensusPrefill2020updated.xlsx")
+?write.xlsx
+
+
+head(potato)
+
